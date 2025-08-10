22301497789bd62012878a4c6b88471513489872
PPchayutt <67070115@kmitl.ac.th>
2025-08-10 22:20:04 +0700

    add 6.5 and 6.6

--- Lab6/lab6_5.c
+++ Lab6/lab6_5.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main() {
-    
+int main()
+{
+    char text[301] = {0};
+    char find;
+    scanf("%[^\n]s", text);
+    scanf(" %c", &find);
+
+    int count = 0;
+    int indices[301] = {0};
+    int endindex = 0;
+
+    for(int i = 0; i < 301; i++){
+        if(text[i] == '\0'){
+            break;
+        }
+        if(tolower(text[i]) == tolower(find)){
+            count++;
+            indices[i] = i + 1;
+            endindex = i;
+        }
+    }
+    if(count > 0){
+        printf("There is/are %d \"%c\" in the above sentences.\n", count, find);
+        printf("Position: ");
+        for(int i = 0; i < 301; i++){
+            if(indices[i] != 0){
+                if(i != endindex) {
+                    printf("%d, ", indices[i]);
+                }
+                else {
+                    printf("%d", indices[i]);
+                    break;
+                }
+            }
+        }
+    }
+    else{
+        printf("Not found.");
+    }
+
     return 0;
 }
--- Lab6/lab6_6.c
+++ Lab6/lab6_6.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main() {
-    
+    char names[20][61];
+    int num_names = 20;
+
+    for (int i = 0; i < num_names; i++) {
+        scanf(" %[^\n]", names[i]);
+    }
+
+    for (int i = 0; i < num_names; i++) {
+        for (int j = 0; names[i][j] != '\0'; j++) {
+            names[i][j] = tolower(names[i][j]);
+        }
+
+        names[i][0] = toupper(names[i][0]);
+
+        for (int j = 0; names[i][j] != '\0'; j++) {
+            if (names[i][j] == ' ') {
+                names[i][j + 1] = toupper(names[i][j + 1]);
+                break;
+            }
+        }
+    }
+
+    char temp[61];
+    for (int i = 0; i < num_names - 1; i++) {
+        for (int j = 0; j < num_names - i - 1; j++) {
+            if (strcmp(names[j], names[j + 1]) > 0) {
+                strcpy(temp, names[j]);
+                strcpy(names[j], names[j + 1]);
+                strcpy(names[j + 1], temp);
+            }
+        }
+    }
+
+    for (int i = 0; i < num_names; i++) {
+        printf("%s\n", names[i]);
+    }
+
     return 0;
 }
